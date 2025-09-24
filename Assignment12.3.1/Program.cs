d1aa14e69d6c9b7c5ddc74236cc2057196fc1e19
ColeKingRogers <rogerscole99@gmail.com>
2025-09-24 15:40:18 -0500

    Add project files.

---
+++ Assignment12.3.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace Assignment12._3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveDuplicates("abbaca"));
+        }
+        public static string RemoveDuplicates(string s)
+        {
+            StringBuilder newS = new StringBuilder(s);
+            for (int i = 0; i < newS.Length -1;)
+            {
+                if (newS[i] == newS[i + 1])
+                {
+                    newS.Remove(i, 2);
+                    if (i == 0) continue;
+                    i--;
+                }
+                else i++;
+            }
+
+            return newS.ToString();
+        }
+    }
+}
